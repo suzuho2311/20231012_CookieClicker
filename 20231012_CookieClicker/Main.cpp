c90c961f8648dfb2ed27046dcc8de9310d131ffe
suzuho2311 <137864885+suzuho2311@users.noreply.github.com>
2023-10-16 09:46:46 +0900

    moji

--- 20231012_CookieClicker/Main.cpp
+++ 20231012_CookieClicker/Main.cpp
@@ -42,43 +42,46 @@ bool Button(const Rect& rect, const Texture& texture, const Font& font, const St
 
 void Main()
 {
-	// クッキーの絵文字
+	// ケーキの絵文字
 	const Texture texture{ U"🍰"_emoji };
 
-	// 農場の絵文字
+	// 普通のケーキ屋の絵文字
 	const Texture farmEmoji{ U"🏠"_emoji };
 
-	// 工場の絵文字
+	// おいしいケーキ屋の絵文字
 	const Texture factoryEmoji{ U"🏡"_emoji };
 
+	// 大人気のケーキ屋の絵文字
 	const Texture cakeEmoji{ U"🏘"_emoji };
 
 	// フォント
 	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
 
-	// クッキーのクリック円
+	// ケーキのクリック円
 	const Circle cookieCircle{ 170, 300, 100 };
 
-	// クッキーの表示サイズ（倍率）
+	// ケーキの表示サイズ（倍率）
 	double cookieScale = 1.5;
 
-	// クッキーの個数
+	// ケーキの個数
 	double cookies = 0;
 
-	// 農場の所有数
+	// 普通のケーキ屋の所有数
 	int32 farmCount = 0;
 
-	// 工場の所有数
+	// おいしいケーキ屋の所有数
 	int32 factoryCount = 0;
 
+	// 大人気のケーキ屋の所有数
 	int32 cakeCount = 0;
 
-	// 農場の価格
+	// 普通のケーキ屋の価格
 	int32 farmCost = 10;
 
-	// 工場の価格
+	// おいしいケーキ屋の価格
 	int32 factoryCost = 100;
 
+	// 大人気のケーキ屋の価格
 	int32 cakeCost = 500;
 
 	// ゲームの経過時間の蓄積
@@ -86,7 +89,7 @@ void Main()
 
 	while (System::Update())
 	{
-		// クッキーの毎秒の生産量 (cookies per second) を計算する
+		// ケーキの毎秒の生産量 (cookies per second) を計算する
 		const int32 cps = (farmCount + (factoryCount * 10) + (cakeCount * 50));
 
 		// ゲームの経過時間を加算する
@@ -97,32 +100,33 @@ void Main()
 		{
 			accumulatedTime -= 0.1;
 
-			// 0.1 秒分のクッキー生産を加算する
+			// 0.1 秒分のケーキ生産を加算する
 			cookies += (cps * 0.1);
 		}
 
-		// 農場の価格を計算する
+		// 普通のケーキ屋の価格を計算する
 		farmCost = 10 + (farmCount * 10);
 
-		// 工場の価格を計算する
+		// おいしいケーキ屋の価格を計算する
 		factoryCost = 100 + (factoryCount * 100);
 
+		// 大人気のケーキ屋の価格を計算する
 		cakeCost = 500 + (cakeCount * 500);
 
-		// クッキー円上にマウスカーソルがあれば
+		// ケーキ円上にマウスカーソルがあれば
 		if (cookieCircle.mouseOver())
 		{
 			Cursor::RequestStyle(CursorStyle::Hand);
 		}
 
-		// クッキー円が左クリックされたら
+		// ケーキ円が左クリックされたら
 		if (cookieCircle.leftClicked())
 		{
 			cookieScale = 1.3;
 			++cookies;
 		}
 
-		// クッキーの表示サイズを回復する
+		// ケーキの表示サイズを回復する
 		cookieScale += Scene::DeltaTime();
 
 		if (1.5 < cookieScale)
@@ -133,29 +137,30 @@ void Main()
 		// 背景を描く
 		Rect{ 0, 0, 800, 600 }.draw(Arg::top = ColorF{ 0.9, 0.5, 0.5 }, Arg::bottom = ColorF{ 1.0, 1.0, 1.0 });
 
-		// クッキーの数を整数で表示する
+		// ケーキの数を整数で表示する
 		font(U"{:.0f}"_fmt(cookies)).drawAt(60, 170, 100);
 
-		// クッキーの生産量を表示する
+		// ケーキの生産量を表示する
 		font(U"毎秒: {}"_fmt(cps)).drawAt(24, 170, 160);
 
-		// クッキーを描画する
+		// ケーキを描画する
 		texture.scaled(cookieScale).drawAt(cookieCircle.center);
 
-		// 農場ボタン
+		// 普通のケーキ屋ボタン
 		if (Button(Rect{ 340, 40, 420, 100 }, farmEmoji, font, U"普通のケーキ屋", U"C{} / 1 CPS"_fmt(farmCost), farmCount, (farmCost <= cookies)))
 		{
 			cookies -= farmCost;
 			++farmCount;
 		}
 
-		// 工場ボタン
+		// おいしいケーキ屋ボタン
 		if (Button(Rect{ 340, 160, 420, 100 }, factoryEmoji, font, U"おいしいケーキ屋", U"C{} / 10 CPS"_fmt(factoryCost), factoryCount, (factoryCost <= cookies)))
 		{
 			cookies -= factoryCost;
 			++factoryCount;
 		}
 
+		// 大人気のケーキ屋ボタン
 		if (Button(Rect{ 340, 280, 420, 100 }, cakeEmoji, font, U"大人気のケーキ屋", U"C{} / 50 CPS"_fmt(cakeCost), cakeCount, (cakeCost <= cookies)))
 		{
 			cookies -= cakeCost;
